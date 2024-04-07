de95b809661e9503a59b5dca80eed7d50b5e8d37
jerry <195658469@qq.com>
2024-04-07 21:03:38 +0800

    ref:update

--- src/window/MainWindow.ts
+++ src/window/MainWindow.ts
@@ -1,5 +1,11 @@
-import { ApplicationWindow, Button, View } from "compass-core";
-import { DragViewHelper } from "../utils/DragViewHelper";
+import {
+  ApplicationWindow,
+  Button,
+  View,
+  TranslateAnimation,
+  Animation,
+} from "compass-core";
+
 import MainWindowLayout from "../res/window/main-window.xml";
 
 export class MainWindow extends ApplicationWindow {
@@ -13,13 +19,24 @@ export class MainWindow extends ApplicationWindow {
     const minions = <View> this.createViewQuery().findViewById("minions");
     const model = <View> this.createViewQuery().findViewById("model");
     const confirm = <Button> this.createViewQuery().findViewById("confirm");
+    const cancel = <Button> this.createViewQuery().findViewById("cancel");
 
     confirm.setOnClickListener({
       onClick(v: View): void {
-        alert("click");
+        alert(0);
       },
     });
-    DragViewHelper.make(model);
-    DragViewHelper.make(minions);
+    cancel.setOnClickListener({
+      onClick(v: View): void {
+        let translateAnimation = new TranslateAnimation(
+          Animation.RELATIVE_TO_SELF, 0,
+          Animation.RELATIVE_TO_SELF, 2,
+          Animation.RELATIVE_TO_SELF, 0,
+          Animation.RELATIVE_TO_SELF, 0,
+        );
+        translateAnimation.setDuration(3000);
+        model.startAnimation(translateAnimation);
+      },
+    });
   }
 }
